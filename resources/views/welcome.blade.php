7e69b5fef5284b6e6b0fe716e4172e722c8a391a
Muhammad Aswan <muhaswan190897@gmail.com>
2022-09-02 09:14:47 +0800

    modify title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Minasa Elektro-Sains</title>
+    <title>PT Minasa Elektro-Sains Teknologi</title>
 </head>
 
 <body>
