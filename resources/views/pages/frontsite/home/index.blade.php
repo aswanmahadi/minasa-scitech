9c68f576e09743fb034d36690e5260fc721f2e15
Muhammad Aswan <muhaswan190897@gmail.com>
2022-11-21 20:08:24 +0800

    fix asset client

--- resources/views/pages/frontsite/home/index.blade.php
+++ resources/views/pages/frontsite/home/index.blade.php
@@ -500,62 +500,74 @@
             <div class="row-client no-gutters clients-wrap  wow fadeInUp">
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-1.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-1.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-2.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-2.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-3.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-3.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-4.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-4.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-5.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-5.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-6.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-6.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-1.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-1.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-2.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-2.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-3.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-3.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-4.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-4.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-5.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-5.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-4 col-6">
                     <div class="client-logo">
-                        <img src="assets/frontsite/img/clients/client-6.png" class="img-fluid" alt="">
+                        <img src="{{ asset('assets/frontsite/img/clients/client-6.png') }}" class="img-fluid"
+                            alt="">
                     </div>
                 </div>
             </div>
