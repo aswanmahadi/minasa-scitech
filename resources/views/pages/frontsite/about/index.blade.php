f55b42f2eac7c7051d6b1a4409218047a8263ab1
Muhammad Aswan <muhaswan190897@gmail.com>
2022-12-08 19:50:19 +0800

    update about page

--- resources/views/pages/frontsite/about/index.blade.php
+++ resources/views/pages/frontsite/about/index.blade.php
@@ -3,6 +3,58 @@
 @section('title', 'About')
 
 @section('content')
+    <!-- About Start -->
+    <div class="container-fluid bg-light overflow-hidden my-5 px-lg-0">
+        <div class="container about px-lg-0">
+            <div class="row g-0 mx-lg-0">
+                <div class="col-lg-6 ps-lg-0 wow fadeIn" data-wow-delay="0.1s" style="min-height: 400px">
+                    <div class="position-relative h-100">
+                        <img class="position-absolute img-fluid w-100 h-100"
+                            src="{{ asset('assets/frontsite/img/about.jpg') }}" style="object-fit: cover" alt="" />
+                    </div>
+                </div>
+                <div class="col-lg-6 about-text py-5 wow fadeIn text-justify" data-wow-delay="0.5s">
+                    <div class="p-lg-5 pe-lg-0">
+                        <h2 class="mb-4 text-primary">
+                            PT Minasa Elektro-Sains Teknologi
+                        </h2>
+                        <p>
+                            Perusahaan yang bergerak dalam bisnis berbasis teknologi yang handal dan terpercaya serta aktif
+                            melakukan kegiatan R&D untuk menghasilkan produk elektronika dan produk ketenagalistrikan.
+                        </p>
+                        <div class="row">
+                            <div class="col-12">
+                                <h4><i class="fa-regular fa-circle-check text-primary"></i> Visi</h4>
+                                <p>Menjadi Perusahaan Pengembang Kendaraan Listrik adan Sistem
+                                    Pembangkit Listrik Berbasis
+                                    Energi Terbarukan yang handal dan terpercaya, serta aktif melakukan kegiatan Research
+                                    and Development untuk menghasilkan produk elektronika dan produk ketenagalistrikan dalam
+                                    rangka mendukung terciptanya kemandirian nasional dan kepercayaan diri bangsa.</p>
+                            </div>
+                            <div class="col-12">
+                                <h4><i class="fa-regular fa-circle-check text-primary"></i> Misi</h4>
+                                <ol>
+                                    <li>Menyediakan jasa penunjang ketenagalistrikan dan menjadi
+                                        perusahaan pengembang
+                                        sistem pembangkit listrik yang berbasis pada pemanfaatan sumber energi terbarukan
+                                        dalam rangka mendukung komitmen global menuju zero emission.</li>
+                                    <li>Aktif melakukan kegiatan R&D untuk menciptakan produk elektronika yang inovatif dan
+                                        kompetitif yang dimanfaatkan pada sistem pembangkitan dan pengolahan tenaga listrik
+                                        berbasis energi terbarukan.</li>
+                                    <li>Aktif melakukan kegiatan R&D untuk menciptakan produk elektronika yang inovatif dan
+                                        kompetitif yang dimanfaatkan pada kendaraan listrik dalam rangka mendukung
+                                        peningkatan tingkat komponen dalam negeri kendaraan listrik nasional.</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- About End -->
+
     <section id="doctors" class="doctors">
         <div class="container-xxl py-5">
 
